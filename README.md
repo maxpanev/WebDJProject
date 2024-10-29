c22b4eca5888a3c502c2ea6c4e0a8ea239250688
maxpanev <maxpanev170100@gmail.com>
2024-10-29 12:49:24 +0300

    add new files

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Домашнее задание лежит в папке ***zerocoder/food***
 ## Lesson DJ02
 Домашнее задание лежит в папке ***zerocoder/main***
+## Lesson DJ03
+Домашнее задание лежит в папке ***zerocoder/news***
--- zerocoder/zerocoder/settings.py
+++ zerocoder/zerocoder/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'news',
     # 'food',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
