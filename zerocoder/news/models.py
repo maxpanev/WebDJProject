75b71205cd76467aa132036d6d7a8b3529751cd1
maxpanev <maxpanev170100@gmail.com>
2024-10-29 20:12:50 +0300

    add new code

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class News_post(models.Model):
     title = models.CharField('Название новости', max_length=50)
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор', null=True, blank=True)
 
     def __str__(self):
         return self.title
